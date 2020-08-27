9dc6c5eff3080a7517912d54aa58581e73647046
TMoweS <t-mowes@hotmail.com>
2020-08-27 06:20:29 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Homepage</title>
+      </Head>
+      <main>
+        <h1>HELLO JULIUS</h1>
+      </main>
+    </div>
+  )
+}
+export default Home
---
+++ src/styles/styled.d.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
+import 'styled-components'
+import theme from './theme'
+export type Theme = typeof theme
+declare module 'styled-components' {
+  export interface DefaultTheme extends Theme {}
+}
